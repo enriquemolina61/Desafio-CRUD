a84628bfe6eb5d3d504716f61dc5ee575ebc6d09
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2022-06-26 12:40:08 -0300

    find all bikes

--- src/controllers/bikeControllers.ts
+++ src/controllers/bikeControllers.ts
@@ -2,7 +2,13 @@ import { Request, Response } from "express";
 import { BikeModel } from "../database/models/BikeModel";
 
 class BikeController {
-  async findAll(req: Request, res: Response) {}
+  async findAll(req: Request, res: Response) {
+    const bikes = await BikeModel.findAll();
+    return bikes.length > 0
+      ? res.status(200).json(bikes)
+      : res.status(204).send();
+  }
+
   async findOne(req: Request, res: Response) {}
   async create(req: Request, res: Response) {
     const { cor, marchas, marca, modelo, preco } = req.body;
