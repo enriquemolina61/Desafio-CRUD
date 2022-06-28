d5e3a00597d97d494a71e4048dabd98d31c28bb9
Enrique Molina <76474854+enriquemolina61@users.noreply.github.com>
2022-06-28 20:16:20 -0300

    try-catch

--- src/controllers/bicycleControllers.ts
+++ src/controllers/bicycleControllers.ts
@@ -5,68 +5,92 @@ const { Op } = require("sequelize");
 
 class BicycleController {
   async findAll(req: Request, res: Response) {
-    const bicycles = await bicycleModel.findAll();
-    return bicycles.length > 0
-      ? res.status(200).json(bicycles)
-      : res.status(204).send();
+    try {
+      const bicycles = await bicycleModel.findAll();
+      return bicycles.length > 0
+        ? res.status(200).json(bicycles)
+        : res.status(204).send();
+    } catch (error) {
+      return res.send({ error });
+    }
   }
 
   async findByColor(req: Request, res: Response) {
-    const { bicycleColor } = req.params;
-    const bicycle = await bicycleModel.findAll({
-      where: {
-        color: bicycleColor,
-      },
-    });
-    return bicycle ? res.status(200).json(bicycle) : res.status(204).send();
+    try {
+      const { bicycleColor } = req.params;
+      const bicycle = await bicycleModel.findAll({
+        where: {
+          color: bicycleColor,
+        },
+      });
+      return bicycle ? res.status(200).json(bicycle) : res.status(204).send();
+    } catch (error) {
+      return res.json({ error });
+    }
   }
 
   async create(req: Request, res: Response) {
-    const { color, gears, brand, model, price } = req.body;
-    const bicycle = await bicycleModel.create({
-      color,
-      gears,
-      brand,
-      model,
-      price,
-    });
-    return res.status(201).json(bicycle);
+    try {
+      const { color, gears, brand, model, price } = req.body;
+      const bicycle = await bicycleModel.create({
+        color,
+        gears,
+        brand,
+        model,
+        price,
+      });
+      return res.status(201).json(bicycle);
+    } catch (error) {
+      return res.send({ error });
+    }
   }
 
   async update(req: Request, res: Response) {
-    const { bicycleId } = req.params;
-    await bicycleModel.update(
-      { price: req.body.price },
-      {
-        where: {
-          id: bicycleId,
-        },
-      }
-    );
-    return res.status(204).send;
+    try {
+      const { bicycleId } = req.params;
+      await bicycleModel.update(
+        { price: req.body.price },
+        {
+          where: {
+            id: bicycleId,
+          },
+        }
+      );
+      return res.status(204).send;
+    } catch (error) {
+      return res.send({ error });
+    }
   }
 
   async sell(req: Request, res: Response) {
-    const { bicycleId } = req.params;
-    await bicycleModel.destroy({
-      where: {
-        id: bicycleId,
-      },
-    });
-    return res.json({ message: "Bicicleta Vendida!" });
+    try {
+      const { bicycleId } = req.params;
+      await bicycleModel.destroy({
+        where: {
+          id: bicycleId,
+        },
+      });
+      return res.json({ message: "Bicicleta Vendida!" });
+    } catch (error) {
+      return res.send(error);
+    }
   }
 
   async findByPrice(req: Request, res: Response) {
-    const { bicyclePrice } = req.params;
-    const bicycle = await bicycleModel.findAll({
-      where: {
-        price: {
-          [Op.lte]: bicyclePrice,
+    try {
+      const { bicyclePrice } = req.params;
+      const bicycle = await bicycleModel.findAll({
+        where: {
+          price: {
+            [Op.lte]: bicyclePrice,
+          },
         },
-      },
-    });
+      });
 
-    return bicycle ? res.status(200).json(bicycle) : res.status(204).send();
+      return bicycle ? res.status(200).json(bicycle) : res.status(204).send();
+    } catch (error) {
+      res.send({ error });
+    }
   }
 }
 
